4c7e409422149de2b93e3b2d455dd8db0595d715
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2023-10-30 01:21:36 +0600

    Update icpc23-preli_mock-b.cpp

--- icpc23-preli_mock-b.cpp
+++ icpc23-preli_mock-b.cpp
@@ -78,5 +78,5 @@ int main(){
 }
 
 // I never got to submit this, I was only learning recursion-backtracking at the moment, so I was happy to come up with a
-// solution using recursion + memorisation. Probably not enough to pass the tests given that seniors from my varsity
+// solution using recursion + memorisation. Probably not enough to pass the tests, given that seniors from my varsity
 // got 10+ WA/TLE before AC, still..
