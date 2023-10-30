f3b4efb76b3522df6a5f1376308c9f4fc75388ee
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2023-10-30 19:23:33 +0600

    Create uva-441.cpp

---
+++ uva-441.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vb = vector<bool>;
+template<class T>
+using indexed_set = tree<T,null_type,less<T>,rb_tree_tag,
+tree_order_statistics_node_update>;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define ins insert
+#define all(x) (x).begin(),(x).end()
+#define sz(y) (int) y.size()
+#define endl "\n"
+#define sim template < class c 
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {  
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {  
+  *this << "[";  
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define db(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+int n;
+vi v;
+
+void bt(int in, int cnt, vi& ans){
+	if(cnt==6){
+		for(int i = 0; i < 6; ++i){
+			cout << ans[i] << ((i==5)?"\n":" ");
+		}
+		return;
+	}
+	for(int i = in; i < n; ++i){
+		ans.pb(v[i]);
+		bt(i+1,cnt+1,ans);
+		ans.pop_back();
+	}
+}
+
+void solve(){
+	int ln = 0;
+	while(cin >> n && n){
+		cout << ((ln)?"\n":"");
+		ln = 1;
+		v.clear();
+		for(int i = 0; i < n; ++i){
+			int x; cin >> x;
+			v.pb(x);
+		}
+		vi ans;
+		bt(0,0,ans);
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	//int tc; cin >> tc;
+	//while(tc--)
+		solve();
+
+	return 0;
+}
